98d0f05bcb818d89f7a817a0b3cad295018bf66f
Jasec <jasec_7@hotmail.com>
2025-08-15 02:31:09 -0700

    complete python p3 fileio lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,22 @@
+def text_path(name):
+        name = str(name)
+        if name.endswith('.txt'):
+            return name
+        else:
+            return name + ".txt"
+
 def write_file(file_name, file_content):
-    pass
+     path = text_path(file_name)
+     with open(path, mode='w', encoding='utf-8') as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
-
+   path = text_path(file_name)
+   with open(path, mode='a', encoding='utf-8') as file:
+    file.write(append_content)
+    
 def read_file(file_name):
-    pass
+    path = text_path(file_name)
+    with open(path, encoding='utf-8') as file:
+        return file.read()
